fae22d6fef2b8614bc5175f96a5915c8ddf70878
Vidhya K <vidhya.k.2202@gmail.com>
2026-09-18 12:43:11 +0530

    Add files via upload

---
+++ Daily Tasks/Day 2 Odd or Even/script.js
@@ -0,0 +1,12 @@
+function OddEven() {
+
+    let number = document.getElementById("numberInput").value;
+
+    if (number % 2 == 0) {
+        document.getElementById("result").innerHTML = "Even Number";
+    }
+    else {
+        document.getElementById("result").innerHTML = "Odd Number";
+    }
+
+}
